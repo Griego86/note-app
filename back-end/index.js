d4dfce37c15a4511a58b25e9d74fe2632b0f4f5d
Griego86 <dimi1086@hotmail.com>
2024-05-08 16:12:39 -0700

    Added databse connection to its own file & installed dotenv

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors =require('cors')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
