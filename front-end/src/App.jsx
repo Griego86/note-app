e9ddbdde969b8cac74b52a14bbb76bf45aa37ad6
Griego86 <dimi1086@hotmail.com>
2024-05-08 11:17:43 -0700

    Added toggle function to make notes important/not important

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -33,6 +33,17 @@ const App = () => {
       })
   }
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changeNote = {...note, important: !note.important}
+
+    axios.put(url, changeNote)
+      .then(response => {
+        setNotes(notes.map(n => n.id !== id ? n : response.data))
+      })
+  }
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
@@ -52,7 +63,11 @@ const App = () => {
    </div>
    <ul>
     {notesToShow.map(note => 
-      <Note key={note.id} note={note} />
+      <Note 
+        key={note.id} 
+        note={note}
+        toggleImportance={() => toggleImportanceOf(note.id)} 
+      />
     )}
    </ul>
    <form onSubmit={addNote}>
