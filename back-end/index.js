cfd6c70bdce3b12a3adfebe61849fabf03017e2f
Griego86 <dimi1086@hotmail.com>
2024-05-08 15:20:40 -0700

    Added static middleware for dist

--- back-end/index.js
+++ back-end/index.js
@@ -4,6 +4,7 @@ const cors =require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -92,7 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
